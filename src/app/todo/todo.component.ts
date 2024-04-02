5dfb35b57b1d822204cec702c4640bf4f708eec6
biel4s <kamilbielawski13@gmail.com>
2024-04-02 16:58:37 +0200

    Added edit button

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
@@ -7,6 +7,11 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+
+export interface ItemsModel {
+  task: string;
+  isEditing: boolean;
+}
 
 @Component({
   selector: 'app-todo',
@@ -24,9 +29,16 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
+
 export class TodoComponent {
+  @ViewChild('editInputField') editInputField: ElementRef | undefined;
+
   todoForm: FormGroup;
-  items: string[] = ['study', 'eat', 'gym'];
+  items: ItemsModel[] = [
+    { task: 'Study', isEditing: false },
+    { task: 'Eat', isEditing: false },
+    { task: 'Gym', isEditing: false },
+  ];
 
   constructor(private fb: FormBuilder) {
     this.todoForm = this.fb.group({
@@ -34,17 +46,26 @@ export class TodoComponent {
     });
   }
 
-  //readonly status = ['to do', 'in progress', 'done'];
-
   addItem(): void {
     const value = this.todoForm.get('task')?.value;
     if (value.trim() !== '') {
-      this.items.push(value);
+      this.items = [...this.items, {task: value, isEditing: false}]
       this.todoForm.reset();
     }
   }
 
-  editItem(index: number): void {}
+  editItem(index: number): void {
+    this.items.forEach((item: ItemsModel, i: number): void => {
+      if (i !== index) {
+        item.isEditing = false;
+      }
+    })
+
+    this.items[index].isEditing = !this.items[index].isEditing;
+    setTimeout((): void => {
+      this.editInputField?.nativeElement.focus();
+    }, 0);
+  }
 
   deleteItem(index: number): void {
     this.items.splice(index, 1);
